a6f5769be4369f74c3ca7f2e42763b7dc6d3f3b0
John Torres <jandrewtorres5@gmail.com>
2018-07-29 19:23:08 -0700

    Side bar widget in place and accessible through command palette.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,58 @@
 import {
-  JupyterLab, JupyterLabPlugin
+  JupyterLab,
+  JupyterLabPlugin,
 } from '@jupyterlab/application';
+
+import {
+  ICommandPalette,
+} from '@jupyterlab/apputils';
+
+import {
+  Widget,
+} from '@phosphor/widgets';
 
 import '../style/index.css';
 
+const FIND_REPLACE_EXT_ID = 'find_replace-extension';
 
 /**
- * Initialization data for the search-replace-notebook-extension extension.
+ * Initialization data for the find-replace-extension.
  */
-const extension: JupyterLabPlugin<void> = {
-  id: 'search-replace-notebook-extension',
+const findReplaceExtension: JupyterLabPlugin<void> = {
+  id: FIND_REPLACE_EXT_ID,
   autoStart: true,
-  activate: (app: JupyterLab) => {
-    console.log('JupyterLab extension search-replace-notebook-extension is activated!');
-  }
+  requires: [ICommandPalette],
+  activate: activateFindReplacePlugin,
 };
 
-export default extension;
+
+function activateFindReplacePlugin(
+  app: JupyterLab,
+  palette: ICommandPalette,
+): void {
+  // Create a single widget
+  let widget: Widget = new Widget();
+  widget.id = FIND_REPLACE_EXT_ID;
+  widget.title.label = 'Find / Replace';
+  widget.title.closable = true;
+
+  // Add an application command
+  const open: string = 'find-replace:open';
+
+  app.commands.addCommand(open, {
+    label: 'Find / Replace',
+    execute: () => {
+      if (!widget.isAttached) {
+        // Attach the widget to the main work area if it's not there
+        app.shell.addToLeftArea(widget, { rank: 800 });
+      }
+      // Activate the widget
+      app.shell.activateById(widget.id);
+    }
+  });
+
+  // Add the command to the palette.
+  palette.addItem({command: open, category: 'File Operations'});
+}
+
+export default findReplaceExtension;
